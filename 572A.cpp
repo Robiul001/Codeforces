80998a53969a1a66b2e569ca9f2436f8efddeaef
Robiul001 <36999139+Robiul001@users.noreply.github.com>
2019-03-14 21:49:13 +0600

    Add files via upload

---
+++ 572A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+      int na,nb,k,m,arra[100005],arrb[100005];
+      cin>>na>>nb>>k>>m;
+      for(int i=0;i<na;i++)
+            cin>>arra[i];
+      for(int i=0;i<nb;i++)
+            cin>>arrb[i];
+      if(arra[k-1]<arrb[nb-m])
+            cout<<"YES"<<endl;
+      else
+            cout<<"NO"<<endl;
+      return 0;
+}
